2e3d5f03cb8749488a438761fd00a01be98f7af3
Mona Lisa <holmesmainsend@gmail.com>
2022-10-25 13:19:41 -0400

    testing integer input from user

---
+++ userinput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter your age: " << endl;
+    cin >> age;
+
+    cout << "You are " << age << " years old" << endl;
+    
+    return 0;
+}
