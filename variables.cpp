d44ba5332a0eaa03d419565f6b94374cf392c491
Mona Lisa <holmesmainsend@gmail.com>
2022-10-25 12:12:38 -0400

    experimenting with strings and print statements

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string characterName = "Roy";
+    int characterAge = 8;
+
+    cout << "This is the story of a boy named " << characterName << endl;
+    cout << "A toy was owned by " << characterName << endl;
+    cout << "This toy brought " << characterName << " joy" << endl;
+    cout << characterName << "'s joy and his toy, boy!" << endl;
+    cout << "At this time he was age " << characterAge << endl;
+
+    return 0;
+}
