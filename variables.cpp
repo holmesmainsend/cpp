6dbf4daff8dba07ec5a77cd930a2886d03d25a6b
Mona Lisa <holmesmainsend@gmail.com>
2022-10-25 12:38:53 -0400

    different tool for new lines

--- variables.cpp
+++ variables.cpp
@@ -9,7 +9,7 @@ int main() {
     cout << "This is the story of a boy named " << characterName << endl;
     cout << "A toy was owned by " << characterName << endl;
     cout << "This toy brought " << characterName << " joy" << endl;
-    cout << characterName << "'s joy and his toy, boy!" << endl;
+    cout << characterName << "'s joy and his toy, boy!\n";
     cout << "At this time he was age " << characterAge << endl;
 
     return 0;
