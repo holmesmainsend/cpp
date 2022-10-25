cd3e4ed318766eac5c48fd4cf58f2f49d23566df
Mona Lisa <holmesmainsend@gmail.com>
2022-10-25 13:22:55 -0400

    testing string input from user

--- userinput.cpp
+++ userinput.cpp
@@ -4,11 +4,17 @@
 using namespace std;
 
 int main() {
+/*
     int age;
     cout << "Enter your age: " << endl;
     cin >> age;
-
     cout << "You are " << age << " years old" << endl;
+*/
+
+    string name;
+    cout << "Enter your name: " << endl;
+    getline(cin, name);
+    cout << "Hello " << name << endl;
     
     return 0;
 }
