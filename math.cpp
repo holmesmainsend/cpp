81685bbb56acfa5714e111a578ede2fa51e1ec45
Mona Lisa <holmesmainsend@gmail.com>
2022-10-25 13:13:59 -0400

    practicing with cmath

--- math.cpp
+++ math.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -9,8 +10,18 @@ int main() {
     cout << 60 + 13 << endl;
     cout << 60 / 13 << endl;
     cout << 60 % 13 << endl;
+    cout << 60.0 / 13 << endl;
     cout << (4 + 3) * 10 << endl;
     cout << 4 + 3 * 10 << endl;
+
+    cout << pow(2, 3) << endl;
+    cout << sqrt(36) << endl;
+    cout << round(4.5) << endl;
+    cout << round(4.499999) << endl;
+    cout << ceil(7.1) << endl;
+    cout << floor(7.9) << endl;
+    cout << fmax(2, 9) << endl;
+    cout << fmin(2, 9) << endl;
 
     return 0;
 }
