8d74475df3180ca7aedef17661e2eaf61ebc05eb
Mona Lisa <holmesmainsend@gmail.com>
2022-10-26 12:52:53 -0400

    added new madlibs file

---
+++ madlibs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+
+    cout << "Roses are red" << endl;
+    cout << "Violets are blue" << endl;
+    cout << "I love you" << endl;
+
+    return 0;
+}
