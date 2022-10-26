965324dc506ece8325b8f83dc1dd3734dc2bbc02
Mona Lisa <holmesmainsend@gmail.com>
2022-10-26 12:47:36 -0400

    added basic calculator prompts and output

--- basiccalc.cpp
+++ basiccalc.cpp
@@ -4,7 +4,13 @@
 using namespace std;
 
 int main() {
+    int num1, num2;
+    cout << "Enter first integer: " << endl;
+    cin >> num1;
+    cout << "Enter second integer: " << endl;
+    cin >> num2;
     
+    cout << num1 + num2 << endl;
 
     return 0;
 }
