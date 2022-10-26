25fc4d70d722c83dc44eec221e7032b4497f93d3
Mona Lisa <holmesmainsend@gmail.com>
2022-10-26 13:41:54 -0400

    added more operators for calc program

--- basiccalc.cpp
+++ basiccalc.cpp
@@ -5,12 +5,23 @@ using namespace std;
 
 int main() {
     double num1, num2;
+    char op;
+
     cout << "Enter first number: " << endl;
     cin >> num1;
     cout << "Enter second number: " << endl;
     cin >> num2;
+    cout << "Enter operator: " << endl;
+    cin >> op;
     
-    cout << num1 + num2 << endl;
+    if (op == '+')
+        cout << num1 + num2 << endl;
+    else if (op == '-')
+        cout << num1 - num2 << endl;
+    else if (op == '/')
+        cout << num1 / num2 << endl;
+    else if (op == '*')
+        cout << num1 * num2 << endl;
 
     return 0;
 }
