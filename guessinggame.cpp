564955ea219f469a2ce4b80f5e626f0e5b9926ed
Mona Lisa <holmesmainsend@gmail.com>
2022-10-27 08:18:24 -0400

    added basic guessing loop

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int main() {
-    
+    int secretNum = 9;
+    int guess;
+
+    while (guess != secretNum) {
+        cout << "Enter your integer guess: " << endl;
+        cin >> guess;
+    }
+
+    cout << "You win!" << endl;
 
     return 0;
 }
