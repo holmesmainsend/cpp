a33be185004669242f8d8d3f03d99bc0eb1fb3d9
Mona Lisa <holmesmainsend@gmail.com>
2022-10-27 09:00:09 -0400

    added first nested for loop

--- arraynest.cpp
+++ arraynest.cpp
@@ -10,7 +10,10 @@ int main() {
         {5, 6}
     };
 
-    cout << numberGrid[2][1] << endl;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 2; j++)
+            cout << numberGrid[i][j] << endl;
+    }
 
     return 0;
 }
