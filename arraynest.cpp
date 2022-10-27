3ae47ea8e0456f8ab3ff4e7dcff2762973b91941
Mona Lisa <holmesmainsend@gmail.com>
2022-10-27 08:54:36 -0400

    added first 2D array

--- arraynest.cpp
+++ arraynest.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main() {
-    
+
+    int numberGrid[3][2] = {
+        {1, 2},
+        {3, 4},
+        {5, 6}
+    };
+
+    cout << numberGrid[2][1] << endl;
 
     return 0;
 }
