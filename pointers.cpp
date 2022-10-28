fc09c21fa46803bdfeeefa42c1d953d3603627c9
Mona Lisa <holmesmainsend@gmail.com>
2022-10-28 19:50:35 -0400

    began adding pointer variables

--- pointers.cpp
+++ pointers.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main() {
     int num = 237;
+    int *pNum = &num;
     double rating = 4.11;
+    double &pRating = &rating;
     string noun = "Book";
+    string *pNoun = &noun;
 
     cout << &num << endl;
     cout << &rating << endl;
