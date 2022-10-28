561bcd0f413dbe1c859247b6eed85f4171752401
Mona Lisa <holmesmainsend@gmail.com>
2022-10-28 19:52:50 -0400

    began adding pointer print statements

--- pointers.cpp
+++ pointers.cpp
@@ -6,13 +6,16 @@ int main() {
     int num = 237;
     int *pNum = &num;
     double rating = 4.11;
-    double &pRating = &rating;
+    double *pRating = &rating;
     string noun = "Book";
     string *pNoun = &noun;
 
     cout << &num << endl;
+    cout << *pNum << endl;
     cout << &rating << endl;
+    cout << *pRating << endl;
     cout << &noun << endl;
+    cout << *pNoun << endl;
     
     return 0;
 }
