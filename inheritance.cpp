faedf64d324cc87e0a617c2985dbc8cb4d3540ae
Mona Lisa <holmesmainsend@gmail.com>
2022-10-29 13:26:36 -0400

    further testing of overriding original superclass attributes

--- inheritance.cpp
+++ inheritance.cpp
@@ -13,14 +13,22 @@ class Automobile {
 };
 
 class Mercedes : public Automobile {
-
+    public:
+        void activateLuxuryMode() {
+            cout << "Luxury mode activating..." << endl;
+        }
+        void brake() {
+            cout << "Reducing speed now..." << endl;
+        }
 };
 
 int main() {
     Automobile car1;
     car1.accelerate();
+    car1.brake();
 
     Mercedes car2;
+    car2.activateLuxuryMode();
     car2.brake();
 
     return 0;
