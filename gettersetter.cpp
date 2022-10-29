684c27d6aa8274b75f584f528ff22ddd181c345a
Mona Lisa <holmesmainsend@gmail.com>
2022-10-29 12:56:48 -0400

    testing getter function

--- gettersetter.cpp
+++ gettersetter.cpp
@@ -11,13 +11,23 @@ class Artwork {
         Artwork(string aTitle, string aArtist, int aYear) {
             title = aTitle;
             artist = aArtist;
-            year = aYear;
+            setYear(aYear);
+        }
+        void setYear(int aYear) {
+            if(aYear > 1199 && aYear < 1600)
+                year = aYear;
+            else 
+                year = 0;
+        }
+        int getYear() {
+            return year;
         }
 };
 
 int main() {
-    Artwork painting1("The Mona Lisa", "Leonardo da Vinci", 1503);
-    cout << painting1.year << endl;
+    Artwork painting1("The Mona Lisa", "Leonardo da Vinci", 1303);
+    cout << painting1.getYear() << endl;
+
 
     return 0;
 }
