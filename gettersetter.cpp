d8199ed461679292b74d0b6c43724facf2d54a1d
Mona Lisa <holmesmainsend@gmail.com>
2022-10-29 12:36:46 -0400

    added artwork constructor class

--- gettersetter.cpp
+++ gettersetter.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+class artwork {
+    public:
+        string title;
+        string artist;
+        int year;
+        artwork(string aTitle, string aArtist, int aYear) {
+            title = aTitle;
+            artist = aArtist;
+            year = aYear;
+        }
+};
+
 int main() {
-    
+
 
     return 0;
 }
