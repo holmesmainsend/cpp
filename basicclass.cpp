8da3d67aa6aa17d87df5d344d6f7201e9ad5eb30
Mona Lisa <holmesmainsend@gmail.com>
2022-10-29 12:30:03 -0400

    added first object function

--- basicclass.cpp
+++ basicclass.cpp
@@ -12,7 +12,13 @@ class Book {
             title = aTitle;
             author = aAuthor;
             pageCount = aPageCount;
-        };
+        }
+
+        bool novella() {
+            if(pageCount < 150)
+                return true;
+            return false;
+        }
 };
 
 int main() {
@@ -21,6 +27,11 @@ int main() {
     cout << "Book Title: " << book1.title << endl;
     cout << "Author: " << book1.author << endl;
     cout << "Page Count: " << book1.pageCount << endl;
+    cout << book1.novella() << endl;
+
+
+    Book book2("The Old Man and The Sea", "Ernest Hemingway", 120);
+    cout << book2.novella() << endl;
 
     return 0;
 }
