fd40b68d599605f71f44b3de7c37c5e7565de89d
Mona Lisa <holmesmainsend@gmail.com>
2022-10-29 12:46:21 -0400

    testing private setting

--- gettersetter.cpp
+++ gettersetter.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 class Artwork {
+    private:
+        int year;
     public:
         string title;
         string artist;
-        int year;
         Artwork(string aTitle, string aArtist, int aYear) {
             title = aTitle;
             artist = aArtist;
@@ -16,7 +17,7 @@ class Artwork {
 
 int main() {
     Artwork painting1("The Mona Lisa", "Leonardo da Vinci", 1503);
-    cout << painting1.artist << endl;
+    cout << painting1.year << endl;
 
     return 0;
 }
