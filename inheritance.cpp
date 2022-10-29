c87ed4894753d197d961116cbc6bdd5ad37ed683
Mona Lisa <holmesmainsend@gmail.com>
2022-10-29 13:20:00 -0400

    added initial constructor

--- inheritance.cpp
+++ inheritance.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+class Automobile {
+    public:
+        void accelerate() {
+            cout << "vrrrrOOOOOOMMMM" << endl;
+        }
+        void brake() {
+            cout << "ERRRRRRRRRTTTT" << endl;
+        }
+};
+
 int main() {
-    
+    Automobile car1;
+    car1.accelerate();
 
     return 0;
 }
